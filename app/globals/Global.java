7d02e171af33d55dd7350d5b1ef5df0d31a68db6
danielburrell <daniel.burrell@gmail.com>
2014-04-28 01:11:08 +0100

    Re-implemented in bootstrap
    Re-implemented the site (badly) in bootstrap.
    Need to clean up the inline style code and insert it back into the CSS

--- app/globals/Global.java
+++ app/globals/Global.java
@@ -1,67 +1,13 @@
 package globals;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-import java.util.Comparator;
-
-import models.Player;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.annotations.Beta;
-import com.google.inject.Guice;
-import com.google.inject.Inject;
-import com.google.inject.Injector;
-
-import controllers.DataFetcher;
-import dao.TourDao;
-import play.Application;
 import play.GlobalSettings;
-import play.Logger;
-import play.Play;
-import play.cache.Cache;
-import play.db.DB;
-import play.libs.Akka;
-import play.libs.Json;
-import play.libs.WS;
-import play.libs.F.Function;
-import play.mvc.Result;
-import scala.concurrent.duration.Duration;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 public class Global extends GlobalSettings {
 
 	private static final Injector INJECTOR = createInjector();
-
-	@Inject
-	private DataFetcher runnable;
-
-	@Override
-	public void onStart(Application arg0) {
-		Logger.info("Scheduling Cache Refresher");
-		INJECTOR.injectMembers(this);
-		Akka.system()
-				.scheduler()
-				.schedule(Duration.create(0, TimeUnit.MILLISECONDS),
-						Duration.create(10, TimeUnit.SECONDS), runnable,
-						Akka.system().dispatcher()
-				);
-	}
 
 	@Override
 	public <A> A getControllerInstance(Class<A> controllerClass)
